adee696a36ca91a7805d4e81a8324ab9e6e1a774
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-07-22 12:09:40 +0330

    Fetching fake data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import Header from "./Header.jsx";
 import Main from "./Main.jsx";
 
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(() => {
+		fetch("http://localhost:8000/questions")
+			.then(res => res.json())
+			.then(data => dispatch({type: "dataReceived", payload: data}))
+			.catch(err => dispatch({type: "dataFailed"}));
+	}, []);
+
 	return (
 		<div className="app">
 			<Header />
