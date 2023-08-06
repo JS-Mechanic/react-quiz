78feb70c80d2d4cd298740230aecbf4cfe886e59
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-06 23:48:07 +0330

    Moving reducer function and state to context file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
-import {useEffect, useReducer} from "react";
 import Loader from "./Loader.jsx";
 import Error from "./Error.jsx";
 import StartScreen from "./StartScreen.jsx";
@@ -10,76 +9,10 @@ import Progress from "./Progress.jsx";
 import FinishScreen from "./FinishScreen.jsx";
 import Footer from "./Footer.jsx";
 import Timer from "./Timer.jsx";
-
-const SECONDS_PER_QUESTION = 20;
-
-const initialState = {
-	questions: [],
-	// loading, error, ready, active, finished
-	status: "loading",
-	index: 0,
-	answer: null,
-	points: 0,
-	highScore: 0,
-	secondsRemaining: null,
-};
-
-function reducer(state, action) {
-	switch (action.type) {
-		case "dataReceived":
-			return {...state, questions: action.payload, status: "ready"};
-		case "dataFailed":
-			return {...state, status: "error"};
-		case "start":
-			return {
-				...state,
-				status: "active",
-				secondsRemaining: state.questions.length * SECONDS_PER_QUESTION,
-			};
-		case "newAnswer":
-			// eslint-disable-next-line no-case-declarations
-			const question = state.questions.at(state.index);
-			return {
-				...state,
-				answer: action.payload,
-				points:
-					action.payload === question.correctOption ? state.points + question.points : state.points,
-			};
-		case "nextQuestion":
-			return {...state, index: state.index + 1, answer: null};
-		case "finish":
-			return {
-				...state,
-				status: "finished",
-				index: action.payload,
-				highScore: state.points > state.highScore ? state.points : state.highScore,
-			};
-		case "restart":
-			return {...initialState, questions: state.questions, status: "active"};
-		case "tick":
-			return {
-				...state,
-				secondsRemaining: state.secondsRemaining - 1,
-				status: state.secondsRemaining === 0 ? "finished" : state.status,
-			};
-		default:
-			throw new Error("Unknown Action!");
-	}
-}
+import {useQuiz} from "../context/QuizContext.jsx";
 
 export default function App() {
-	const [{questions, status, index, answer, points, highScore, secondsRemaining}, dispatch] =
-		useReducer(reducer, initialState);
-	const numQuestions = questions.length;
-	const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
-
-	useEffect(() => {
-		fetch("http://localhost:8000/questions")
-			.then(res => res.json())
-			.then(data => dispatch({type: "dataReceived", payload: data}))
-			.catch(() => dispatch({type: "dataFailed"}));
-	}, []);
-
+	const {status} = useQuiz();
 	return (
 		<div className="app">
 			<Header />
